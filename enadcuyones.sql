b95d3d0368f2421daa53b156a0b4b58a8ff9a4a9
Fiel Enad <111168128+Yongzx1@users.noreply.github.com>
2025-03-12 14:46:59 +0800

    Add files via upload

--- enadcuyones.sql
+++ enadcuyones.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2025 at 04:57 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Mar 12, 2025 at 07:45 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,23 +33,24 @@ CREATE TABLE `cars` (
   `model` varchar(255) NOT NULL,
   `year` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `image_url` varchar(255) DEFAULT NULL
+  `image_url` varchar(255) DEFAULT NULL,
+  `availability` enum('available','sold','reserved') NOT NULL DEFAULT 'available'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`carId`, `brand`, `model`, `year`, `price`, `image_url`) VALUES
-(18, 'NISSAN', 'SKYLINE R34 GTR', 2002, 1036000.00, 'uploads/skyliner34.jpg'),
-(19, 'TOYOTA', 'SUPRA MK4', 1998, 120000.00, 'uploads/mk4supra.jpg'),
-(20, 'MAZDA', 'RX-7 FD3S', 1995, 85000.00, 'uploads/mazdarx7.jpg'),
-(21, 'HONDA', 'NSX NA1', 1997, 150000.00, 'uploads/hondansx.jpg'),
-(22, 'SUBARU', 'IMPREZA 22B STI', 1998, 250000.00, 'uploads/subaruimpreza.jpg'),
-(23, 'MITSUBISHI', 'LANCE EVOLUTION VI TME ', 1999, 90000.00, 'uploads/evo6.jpg'),
-(43, 'NISSAN', 'SILVIA S15 SPEC-R', 1999, 25000.00, 'uploads/s15specr.jpg'),
-(44, 'Toyota', ' Chaser JZX100 Tourer V', 1996, 18000.00, 'uploads/chaserjzx.jpg'),
-(45, 'Honda', 'Integra Type R (DC2)', 1998, 22000.00, 'uploads/integratyper.jpg');
+INSERT INTO `cars` (`carId`, `brand`, `model`, `year`, `price`, `image_url`, `availability`) VALUES
+(18, 'NISSAN', 'SKYLINE R34 GTR', 2002, 1036000.00, 'uploads/skyliner34.jpg', 'sold'),
+(19, 'TOYOTA', 'SUPRA MK4', 1998, 120000.00, 'uploads/mk4supra.jpg', 'reserved'),
+(20, 'MAZDA', 'RX-7 FD3S', 1995, 85000.00, 'uploads/mazdarx7.jpg', 'available'),
+(21, 'HONDA', 'NSX NA1', 1997, 150000.00, 'uploads/hondansx.jpg', 'available'),
+(22, 'SUBARU', 'IMPREZA 22B STI', 1998, 250000.00, 'uploads/subaruimpreza.jpg', 'available'),
+(23, 'MITSUBISHI', 'LANCE EVOLUTION VI TME ', 1999, 90000.00, 'uploads/evo6.jpg', 'available'),
+(43, 'NISSAN', 'SILVIA S15 SPEC-R', 1999, 25000.00, 'uploads/s15specr.jpg', 'available'),
+(44, 'Toyota', ' Chaser JZX100 Tourer V', 1996, 18000.00, 'uploads/chaserjzx.jpg', 'available'),
+(45, 'Honda', 'Integra Type R (DC2)', 1998, 22000.00, 'uploads/integratyper.jpg', 'available');
 
 -- --------------------------------------------------------
 
@@ -78,8 +79,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userId`, `firstName`, `lastName`, `email`, `password`, `phoneNumber`, `gender`, `role`, `birthday`, `verification`, `profilePicture`, `createdAt`) VALUES
 (1, 'andrew', 'villalo', 'andrew@email.com', '123', '123', 'Male', 'admin', '2025-03-10', 0, NULL, '2025-03-03 17:10:10'),
-(4, 'fiel', 'enad', 'fiel@gmail.com', '123', '09123', 'Male', 'admin', '2025-03-04', 0, NULL, '2025-03-04 10:51:58'),
-(6, 'yong', 'gwapo', 'yong@gmail.com', '123', '09123456', 'Male', 'user', '0000-00-00', 0, NULL, '2025-03-04 23:05:27'),
+(4, 'fielzxc', 'enad', 'fiel@gmail.com', '123', '0912322', 'Male', 'admin', '2025-03-04', 0, NULL, '2025-03-04 10:51:58'),
+(6, 'yong', 'gwapo1', 'yong@gmail.com', '123', '09123456', 'Male', 'user', '0000-00-00', 0, NULL, '2025-03-04 23:05:27'),
 (7, 'steph', 'curry', 'steph@gmail.com', '123', '091235437', 'Male', 'user', '0000-00-00', 0, NULL, '2025-03-10 15:39:52');
 
 --
